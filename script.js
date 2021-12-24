60bfcbae8fb0945f7cc2ce966e491b5d3bda25e1
Myhardcore <nik-vna@mail.ru>
2021-12-24 18:48:06 +0300

    1.0

---
+++ script.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+   
+    // Navigation arrows
+    navigation: {
+        nextEl: '.button-next',
+        prevEl: '.button-prev',
+    },
+    
+    
+  });
